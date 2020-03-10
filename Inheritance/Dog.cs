a9d60c9faf4f4e58b319a35fd5b9dbb7d36ae141
PeterShipp <pshipp81@aol.com>
2020-03-10 11:59:31 -0700

    Add project files.

---
+++ Inheritance/Dog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Dog : Animals
+    {
+        private string favoriteChewToy;
+
+        public Dog (string name, int numOfLegs, int numOfArms, bool isDangerous, bool isFurry, string favoriteChewToy)
+        {
+            //stuff from our base class, Animal
+            this.name = name;
+            this.numOfLegs = 4;
+            this.numOfArms = 0;
+            this.isDangerous = isDangerous;
+            this.isFurry = isFurry;
+
+            //new stuff only our Dog can do....
+            this.favoriteChewToy = favoriteChewToy;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("The" + this.GetType() + " goes WOOF WOOF!");
+        }
+
+        public override int Sell()
+        {
+            Console.WriteLine("I love my dog and I would never sell her");
+            return 0;
+        }
+    }
+}
